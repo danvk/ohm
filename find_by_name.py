845de7f8825bf7cb98cfaee565bbd46e01f4f7fa
Dan Vanderkam <danvdk@gmail.com>
2026-02-19 12:30:31 -0500

    re.match

--- find_by_name.py
+++ find_by_name.py
@@ -1,4 +1,5 @@
 import argparse
+import re
 import sys
 from typing import Any
 
@@ -8,22 +9,25 @@ import osmium
 class NameFinder(osmium.SimpleHandler):
     def __init__(self, search_name: str):
         super(NameFinder, self).__init__()
-        self.search_name = search_name
+        self.search_re = re.compile(search_name)
+
+    def is_match(self, name: str) -> bool:
+        return re.match(self.search_re, name) is not None
 
     def node(self, n: Any) -> None:
-        if n.tags.get("name") == self.search_name:
+        if self.is_match(n.tags.get("name")):
             self.print_object("node", n)
 
     def way(self, w: Any) -> None:
-        if w.tags.get("name") == self.search_name:
+        if self.is_match(w.tags.get("name")):
             self.print_object("way", w)
 
-    def relation(self, r: Any) -> None:
-        if r.tags.get("name") == self.search_name:
+    def relation(self, r) -> None:
+        if self.is_match(r.tags.get("name")):
             self.print_object("relation", r)
 
     def print_object(self, type_name: str, obj: Any) -> None:
-        print(f"Found {type_name}: {obj.id} (version={obj.version})")
+        print(f"Found {type_name}/{obj.id} {obj.tags.get('name')}")
 
 
 def main() -> None:
