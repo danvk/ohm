5bd9e30e658249041226247e57438fdac7f91a3c
Dan Vanderkam <danvdk@gmail.com>
2026-02-27 16:17:17 -0500

    attempt to wire up react component

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,53 +1,10 @@
-import React, { useEffect, useRef } from 'react';
-import maplibregl, { StyleSpecification } from 'maplibre-gl';
-import 'maplibre-gl/dist/maplibre-gl.css';
+import React from 'react';
+import { MapLibreMap } from './MapLibreMap';
 
-const MINIMAL_STYLE: StyleSpecification = {
-  version: 8,
-  sources: {
-    maplibre: {
-      type: 'vector',
-      url: 'https://demotiles.maplibre.org/tiles/tiles.json',
-    },
-  },
-  layers: [
-    {
-      id: 'background',
-      type: 'background',
-      paint: { 'background-color': '#e8e8e8' },
-    },
-    {
-      id: 'countries-fill',
-      type: 'fill',
-      source: 'maplibre',
-      'source-layer': 'countries',
-      paint: { 'fill-color': '#aaaaaa' },
-    },
-
-  ],
-};
+const DEFAULT_CENTER: [number, number] = [0, 20];
 
 export default function App() {
-  const mapContainer = useRef<HTMLDivElement>(null);
-  const map = useRef<maplibregl.Map | null>(null);
-
-  useEffect(() => {
-    if (map.current || !mapContainer.current) return;
-
-    map.current = new maplibregl.Map({
-      container: mapContainer.current,
-      style: MINIMAL_STYLE,
-      center: [0, 20],
-      zoom: 1.5,
-    });
-
-    map.current.addControl(new maplibregl.NavigationControl(), 'top-right');
-
-    return () => {
-      map.current?.remove();
-      map.current = null;
-    };
-  }, []);
-
-  return <div ref={mapContainer} style={{ width: '100vw', height: '100vh' }} />;
+  return (<div style={{ width: '100vw', height: '100vh' }}>
+    <MapLibreMap center={DEFAULT_CENTER} zoom={1.5} />
+  </div>);
 }
---
+++ app/src/MapLibreMap.tsx
@@ -0,0 +1,109 @@
+import React, {
+  useEffect,
+  useRef,
+  useState,
+  createContext,
+  useContext,
+} from 'react';
+import maplibregl from 'maplibre-gl';
+import 'maplibre-gl/dist/maplibre-gl.css';
+
+const MINIMAL_STYLE: maplibregl.StyleSpecification = {
+  version: 8,
+  sources: {
+    maplibre: {
+      type: 'vector',
+      url: 'https://demotiles.maplibre.org/tiles/tiles.json',
+    },
+  },
+  layers: [
+    {
+      id: 'background',
+      type: 'background',
+      paint: { 'background-color': '#e8e8e8' },
+    },
+    {
+      id: 'countries-fill',
+      type: 'fill',
+      source: 'maplibre',
+      'source-layer': 'countries',
+      paint: { 'fill-color': '#aaaaaa' },
+    },
+
+  ],
+};
+
+interface MapLibreMapProps extends Partial<maplibregl.MapOptions> {
+  containerId?: string;
+  containerClassName?: string;
+  onClick?: () => void;
+  children?: React.JSX.Element | React.JSX.Element[];
+}
+
+const MapContext = createContext<maplibregl.Map | undefined>(undefined);
+
+export function useMap() {
+  return useContext(MapContext);
+}
+
+export function MapLibreMap({
+  children,
+  onClick,
+  containerId,
+  containerClassName,
+  ...mapOptions
+}: MapLibreMapProps) {
+  const mapContainerRef = useRef<HTMLDivElement>(null);
+  const [mapRef, setMapRef] = useState<maplibregl.Map | undefined>();
+  const [initialOptions] = useState<typeof mapOptions>(mapOptions);
+
+  useEffect(() => {
+    if (!mapContainerRef.current) return;
+
+    const map = new maplibregl.Map({
+      container: mapContainerRef.current,
+      style: MINIMAL_STYLE,
+      dragRotate: false,
+      rollEnabled: false,
+      pitchWithRotate: false,
+      ...initialOptions,
+    });
+    map.on('style.load', () => {
+      // It's dangerous to interact with the map before the style is done loading.
+      setMapRef(map);
+    });
+
+    return () => {
+      map.remove();
+    };
+  }, [initialOptions]);
+
+  // TODO: other options should be reactive, too
+  const { center } = mapOptions;
+  React.useEffect(() => {
+    if (center) {
+      mapRef?.setCenter(center);
+    }
+  }, [center, mapRef]);
+
+  React.useEffect(() => {
+    if (onClick) {
+      mapRef?.on('click', onClick);
+      return () => {
+        mapRef?.off('click', onClick);
+      };
+    }
+  }, [mapRef, onClick]);
+
+  // TODO: maybe another wrapper div would be safer -- setting containerClassName here is dangerous.
+  return (
+    <>
+      <div
+        ref={mapContainerRef}
+        id={containerId}
+        className={containerClassName}
+      />
+      <MapContext.Provider value={mapRef}>{children}</MapContext.Provider>
+    </>
+  );
+}
