9eb2b54c1f5dbae90886469317d2ab4053d6c1e4
Dan Vanderkam <danvdk@gmail.com>
2026-03-31 12:38:10 -0400

    fetch examples

--- stats-pipeline.sh
+++ stats-pipeline.sh
@@ -5,9 +5,13 @@ set -x
 date=$1  # 2026-03-01
 yymmdd=${date//-/}
 yymmdd=${yymmdd/#20/}  # 260301
+
+s3cmd get s3://planet.openhistoricalmap.org/planet/planet-260331*
 planet=planet-${yymmdd}_*.osm.pbf
 
 dir=stats/$date
 mkdir -p $dir
 uv run chrono_stats.py $planet --output_dir $dir
 uv run bad_geometry.py $planet --output_dir $dir
+
+rm $planet
--- stats/chart.js
+++ stats/chart.js
@@ -5,19 +5,37 @@ const header = headerRow.split(",");
 const rows = rowStrs.map((rs) => rs.split(","));
 
 const METRIC_DOCS = {
-  'date': {label: 'Date', help: 'Date of planet dump file'},
-  "nonclosed-ring": {label: 'Non-closed Rings', help: 'At least one ring (inner or outer) in the relation could not be closed. It\'s likely that a way is missing.'},
-  "ring-self-intersect": {label: 'Ring Self-intersects', help: 'At least one ring (inner or outer) in the relation intersects itself.'},
-  "self-intersect": {label: 'Self-intersects', help: 'Two rings in the relation cross each other.'},
-  "uncontained-inner-ring": {label: 'Uncontainer inner', help: 'A role=inner ring is not contained in any outer ring.'},
-  "nested-shells": {label: 'Nested Rings', help: 'One ring is contained inside another without role=inner.'},
-  "no-shapely": {label: 'Shapely Error', help: 'Shapely was unable to create a geometry for this relation.'},
-  "other": {label: 'Other', help: 'A grab bag of all other geometry errors.'},
-  "chronology-member-outside-range": {label: 'Member Outside Parent Range'},
-  "chronology-overlapping-members": {label: 'Overlapping Members'},
-  "chronology-undated-member": {label: 'Undated Members'},
-  "dates-in-names": {label: 'Date Ranges in name'},
-  "invalid-date": {label: 'Invalid Dates'}
+  date: { label: "Date", help: "Date of planet dump file" },
+  "nonclosed-ring": {
+    label: "Non-closed Rings",
+    help: "At least one ring (inner or outer) in the relation could not be closed. It's likely that a way is missing.",
+  },
+  "ring-self-intersect": {
+    label: "Ring Self-intersects",
+    help: "At least one ring (inner or outer) in the relation intersects itself.",
+  },
+  "self-intersect": {
+    label: "Self-intersects",
+    help: "Two rings in the relation cross each other.",
+  },
+  "uncontained-inner-ring": {
+    label: "Uncontainer inner",
+    help: "A role=inner ring is not contained in any outer ring.",
+  },
+  "nested-shells": {
+    label: "Nested Rings",
+    help: "One ring is contained inside another without role=inner.",
+  },
+  "no-shapely": {
+    label: "Shapely Error",
+    help: "Shapely was unable to create a geometry for this relation.",
+  },
+  other: { label: "Other", help: "A grab bag of all other geometry errors." },
+  "chronology-member-outside-range": { label: "Member Outside Parent Range" },
+  "chronology-overlapping-members": { label: "Overlapping Members" },
+  "chronology-undated-member": { label: "Undated Members" },
+  "dates-in-names": { label: "Date Ranges in name" },
+  "invalid-date": { label: "Invalid Dates" },
 };
 
 function dataForSeries(...series) {
@@ -29,14 +47,17 @@ function dataForSeries(...series) {
     }
     idxs.push(idx);
   }
-  const lastRow = [''].concat(rows.at(-1).slice(1).map(v => Number(v)));
+  const lastRow = [""].concat(
+    rows
+      .at(-1)
+      .slice(1)
+      .map((v) => Number(v)),
+  );
   idxs.sort((a, b) => lastRow[b] - lastRow[a]);
-  const allIdxs = [0].concat(idxs);  // always include the date
+  const allIdxs = [0].concat(idxs); // always include the date
 
-  const sliceRows = [
-    allIdxs.map(idx => METRIC_DOCS[header[idx]].label)
-  ];
-  for (const fullRow of rows) {
+  const sliceRows = [];
+  for (const fullRow of [header].concat(rows)) {
     const row = allIdxs.map((idx) => fullRow[idx]);
     sliceRows.push(row);
   }
@@ -45,33 +66,57 @@ function dataForSeries(...series) {
 }
 
 function makeChart(container, ...series) {
-  const chartEl = container.querySelector('.chart');
-  const labelsEl = container.querySelector('.chart-labels');
-  const g = new Dygraph(
-    chartEl,
-    dataForSeries(
-      ...series
-    ),
-    {
-      labelsDiv: labelsEl,
-      labelsSeparateLines: true,
-      legend: 'always',
-      xRangePad: 5,
-      labelsKMB: true,
-      hideOverlayOnMouseOut: false,
+  const chartEl = container.querySelector(".chart");
+  const labelsEl = container.querySelector(".chart-labels");
+  const g = new Dygraph(chartEl, dataForSeries(...series), {
+    labelsDiv: labelsEl,
+    labelsSeparateLines: true,
+    legend: "always",
+    xRangePad: 5,
+    labelsKMB: true,
+    hideOverlayOnMouseOut: false,
+    legendFormatter: (data) => {
+      if (data.x == null) {
+        // This should only be a temporary state, until the setSelection() kicks in.
+        return '';
+      }
+
+      console.log(data);
+      var html = data.xHTML;
+      const rawDate = rows[data.i][0];
+      data.series.forEach(function (series) {
+        if (!series.isVisible) return;
+        const {label, help} = METRIC_DOCS[series.label];
+        const labelHtml = `<span style="color: ${series.color}" title="${help}">${label}</span>: <a data-date="${rawDate}" data-series="${series.label}" class="count" href="#">${series.y}</a>`;
+        html += `<br>${series.dashHTML} ${labelHtml}`;
+      });
+      return html;
     },
-  );
+  });
   g.setSelection(rows.length - 1);
+
+  const examplesEl = document.querySelector('.examples');
+  labelsEl.addEventListener('click', async e => {
+    const a = e.target;
+    const date = a.getAttribute('data-date');
+    const metric = a.getAttribute('data-series');
+    console.log(date, metric);
+    const r = await fetch(`./${date}/${metric}.examples.txt`);
+    const text = await r.text();
+    console.log(text);
+    examplesEl.textContent = text;
+  })
 }
 
-makeChart(document.getElementById('geometry-errors'),
- "nonclosed-ring",
- "ring-self-intersect",
- "self-intersect",
- "uncontained-inner-ring",
- "nested-shells",
- "no-shapely",
- "other"
+makeChart(
+  document.getElementById("geometry-errors"),
+  "nonclosed-ring",
+  "ring-self-intersect",
+  "self-intersect",
+  "uncontained-inner-ring",
+  "nested-shells",
+  "no-shapely",
+  "other",
 );
 
 makeChart(
@@ -84,5 +129,5 @@ makeChart(
 makeChart(
   document.getElementById("tag-errors"),
   "dates-in-names",
-  "invalid-date"
+  "invalid-date",
 );
