0ef08f483aa54232ffaba01bf9baef82a697dbf0
Dan Vanderkam <danvdk@gmail.com>
2026-04-02 13:01:24 -0400

    ruff

--- stats.py
+++ stats.py
@@ -14,19 +14,19 @@ def write_stats(
         out = csv.DictWriter(f, fieldnames=["type", "count"])
         out.writeheader()
         out.writerows(
-            {"type": typ, "count": count}
-            for typ, count in sorted(other_stats.items())
+            {"type": typ, "count": count} for typ, count in sorted(other_stats.items())
         )
         out.writerows(
-            {"type": typ, "count": len(rs)}
-            for typ, rs in sorted(examples.items())
+            {"type": typ, "count": len(rs)} for typ, rs in sorted(examples.items())
         )
 
     rng = random.Random(2026)
     for typ, rs in sorted(examples.items()):
         with open(out_dir / f"{typ}.examples.txt", "w") as f:
             rs_sorted = sorted(rs)
-            to_out = rs_sorted if len(rs_sorted) < 5_000 else rng.sample(rs_sorted, 1_000)
+            to_out = (
+                rs_sorted if len(rs_sorted) < 5_000 else rng.sample(rs_sorted, 1_000)
+            )
             f.writelines(
                 f"{ftype}/{fid}: {problems}\n" for ftype, fid, problems in to_out
             )
