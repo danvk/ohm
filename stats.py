de00cd51045ebeeffa63c56a7c4875809b9ab7ba
Dan Vanderkam <danvdk@gmail.com>
2026-04-02 12:58:19 -0400

    stabilize output

--- stats.py
+++ stats.py
@@ -14,13 +14,19 @@ def write_stats(
         out = csv.DictWriter(f, fieldnames=["type", "count"])
         out.writeheader()
         out.writerows(
-            {"type": typ, "count": count} for typ, count in other_stats.items()
+            {"type": typ, "count": count}
+            for typ, count in sorted(other_stats.items())
         )
-        out.writerows({"type": typ, "count": len(rs)} for typ, rs in examples.items())
+        out.writerows(
+            {"type": typ, "count": len(rs)}
+            for typ, rs in sorted(examples.items())
+        )
 
-    for typ, rs in examples.items():
+    rng = random.Random(2026)
+    for typ, rs in sorted(examples.items()):
         with open(out_dir / f"{typ}.examples.txt", "w") as f:
-            to_out = rs if len(rs) < 5_000 else random.sample(rs, 1_000)
+            rs_sorted = sorted(rs)
+            to_out = rs_sorted if len(rs_sorted) < 5_000 else rng.sample(rs_sorted, 1_000)
             f.writelines(
                 f"{ftype}/{fid}: {problems}\n" for ftype, fid, problems in to_out
             )
