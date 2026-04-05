a714be9dca069c3b8c8a87cf85fe43c575bc6684
Dan Vanderkam <danvdk@gmail.com>
2026-04-05 11:05:57 -0400

    Detect bad planet files, rework raw features chart (#15)
    * detect empty ways and abort
    
    * split out raw feature count charts
    
    * consider all ways, not just named ways
    
    * index raw features to 1
    
    * final scaled chart

--- feature_stats.py
+++ feature_stats.py
@@ -1,8 +1,27 @@
 import argparse
 import json
 import subprocess
+import sys
+
+import osmium
+import osmium.osm
 
 from stats import write_stats
+
+
+def count_empty_ways(osm_file: str) -> int:
+    fp = osmium.FileProcessor(osm_file, osmium.osm.WAY)
+    n_ways = 0
+    n_empty_ways = 0
+    for obj in fp:
+        if not isinstance(obj, osmium.osm.Way):
+            continue
+        n_ways += 1
+        if len(obj.nodes) == 0:
+            n_empty_ways += 1
+
+    sys.stderr.write(f"{osm_file}: {n_ways=} {n_empty_ways=}\n")
+    return n_empty_ways
 
 
 def main() -> None:
@@ -12,6 +31,12 @@ def main() -> None:
     parser.add_argument("osm_file", help="Path to the .osm.pbf file")
     parser.add_argument("--output_dir", default=".")
     args = parser.parse_args()
+
+    n_empty_ways = count_empty_ways(args.osm_file)
+    if n_empty_ways > 0:
+        sys.stderr.write(f"{args.osm_file} has empty ways and is likely corrupt.\n")
+        sys.stderr.write("No data will be written.\n")
+        sys.exit(1)
 
     out = subprocess.check_output(
         [
