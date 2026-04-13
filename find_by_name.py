d4092a9d893ac56f6cea8c52deedb2a978cefb90
Dan Vanderkam <danvdk@gmail.com>
2026-04-13 08:48:49 -0400

    search name:en in addition to name

--- find_by_name.py
+++ find_by_name.py
@@ -5,6 +5,7 @@ from typing import Any
 
 import osmium
 import osmium.filter
+from osmium.osm import Node, OSMObject, Relation, Way
 
 
 class NameFinder(osmium.SimpleHandler):
@@ -12,23 +13,33 @@ class NameFinder(osmium.SimpleHandler):
         super(NameFinder, self).__init__()
         self.search_re = re.compile(search_name)
 
-    def is_match(self, name: str) -> bool:
-        return re.match(self.search_re, name) is not None
+    def is_match(self, o: OSMObject) -> bool:
+        name = o.tags.get("name")
+        if name and re.match(self.search_re, name):
+            return True
+        name = o.tags.get("name:en")
+        if name and re.match(self.search_re, name):
+            return True
+        return False
 
-    def node(self, n: Any) -> None:
-        if self.is_match(n.tags.get("name")):
+    def node(self, n: Node) -> None:
+        if self.is_match(n):
             self.print_object("node", n)
 
-    def way(self, w: Any) -> None:
-        if self.is_match(w.tags.get("name")):
+    def way(self, w: Way) -> None:
+        if self.is_match(w):
             self.print_object("way", w)
 
-    def relation(self, r) -> None:
-        if self.is_match(r.tags.get("name")):
+    def relation(self, r: Relation) -> None:
+        if self.is_match(r):
             self.print_object("relation", r)
 
     def print_object(self, type_name: str, obj: Any) -> None:
-        print(f"Found {type_name}/{obj.id} {obj.tags.get('name')}")
+        name = obj.tags.get("name")
+        name_en = obj.tags.get("name:en")
+        if name_en == name:
+            name_en = None
+        print(f"Found {type_name}/{obj.id} {name} {name_en}")
 
 
 def main() -> None:
